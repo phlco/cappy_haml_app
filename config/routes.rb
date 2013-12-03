e4dea8a8bf38866441ad502c48c2ffbd6ebe5cbf
Edward Shin <Eshin0910@gmail.com>
2013-12-03 10:30:35 -0500

    HAML testing working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CappyHamlApp::Application.routes.draw do
   root to: 'welcome#index'
+  get '/haml' => 'haml#index'
+  get '/quiz' => 'quiz#index'
 
 
   # The priority is based upon order of creation:
